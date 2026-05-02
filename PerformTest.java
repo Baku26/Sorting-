8b9657793e847ca6a12b412575779c48ff2e7caa
Baku26 <bakunasa@go.stockton.edu>
2026-05-01 23:16:16 -0400

    Implement array and ArrayList filling in PerformTest
    Added array and ArrayList population methods to performance test.

--- PerformTest.java
+++ PerformTest.java
@@ -5,6 +5,7 @@ public class PerformTest {
         for(int size = 1000; size <= 100000; size += 1000) {
             
             int[] arr = new int[size];
+            Perform.fillArray(arr);
             long startTime = System.currentTimeMillis();
             
             long endTime = System.currentTimeMillis();
@@ -13,7 +14,7 @@ public class PerformTest {
             
             ArrayList<Integer> list = new ArrayList<>();
             startTime = System.currentTimeMillis();
-            
+             Perform.fillArrayList(list, size); 
             endTime = System.currentTimeMillis();
             System.out.println("arrayList | size:" + size + "time:" + (endTime - startTime) + "ms");
         
