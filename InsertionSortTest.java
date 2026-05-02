c859a235d51c74fece208497bf36dc7b1b52d363
Baku26 <bakunasa@go.stockton.edu>
2026-05-02 12:00:31 -0400

    Update InsertionSortTest.java

--- InsertionSortTest.java
+++ InsertionSortTest.java
@@ -4,24 +4,13 @@ public class InsertionSortTest {
       int data[] = {7, 3, 0, 2, 8};
         long totalStart = System.nanoTime();
       InsertionSort.Sort(data);
-      
+        long endTime = System.nanoTime();
+      System.out.print("sorted array list");
       for(int num : data) {
           System.out.println(num + " ");
 
-          long constStart = System.nanoTime();
-        
-        long constEnd = System.nanoTime();
-        
-        long searchStart = System.nanoTime();
-       
-        long searchEnd = System.nanoTime();
-        
-      
-        long totalEnd = System.nanoTime();
-        
-        System.out.println("construction : " + (constEnd - constStart) / 1e6 + "ms");
-        System.out.println("searching :" + (searchEnd - searchStart) / 1e6 + "ms");
-        System.out.println("Total:" + (totalEnd - totalStart) / 1e6 + "ms");
+        double durationsMs = ( endTime - startTime) / 1_000_000;
+           System.out.println("\n\nTotal Sort Time: " + durationMs + " ms");
           
       }
       }
